1f1fb4229349d81b1464680c1bdd8b7241b5e07d
mecaneer23 <mecaneer23@gmail.com>
2023-04-14 17:34:49 -0500

    hmmmm wip paths

--- count_down_from.sh
+++ count_down_from.sh
@@ -1,5 +1,5 @@
 count_down_from() {
-	alacritty -o font.size=30 -o window.title="Count Down From $1" -e /path/to/count_down_from_ $1
+	alacritty -o font.size=30 -o window.title="Count Down From $2" -e "$1"count_down_from_ $2
 }
 
-count_down_from $1
+count_down_from $1 $#
